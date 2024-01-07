36feb4bd9045902f64392deebc92ec09e2830475
nevuos <seganfredofelipe@gmail.com>
2024-01-07 12:56:38 -0300

    Initial Commit

---
+++ src/main/java/com/upf/br/aplicationTest/AplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.upf.br.aplicationTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upf/br/aplicationTest/AplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upf.br.aplicationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
